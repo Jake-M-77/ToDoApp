ba616242f8b4f138ae350966611870f8cd0e235b
Jake M <jakemanson77@icloud.com>
2025-08-24 11:47:38 +0100

    feat: add launchpad and todo creator page, setup API project, and refactor Program.cs

---
+++ Todo_API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger only in development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
