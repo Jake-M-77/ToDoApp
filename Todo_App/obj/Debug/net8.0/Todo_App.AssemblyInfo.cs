e304a8969f739ab6f6f291815aea3854c507eb9b
Jake M <jakemanson77@icloud.com>
2025-08-31 15:02:16 +0100

    Search function added to both todolist pages

--- Todo_App/obj/Debug/net8.0/Todo_App.AssemblyInfo.cs
+++ Todo_App/obj/Debug/net8.0/Todo_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a1a9dba17a2e3f229eede862f6851f50551318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91135d64e8895d05ce178a79ce0980c11ff3101d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
