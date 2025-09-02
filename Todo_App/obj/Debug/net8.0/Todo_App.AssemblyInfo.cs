c3d6e029f41dbd26095e523f429d7d81c5193748
Jake M <jakemanson77@icloud.com>
2025-09-02 13:17:35 +0100

    Added redirect to launchpad on todo creator page

--- Todo_App/obj/Debug/net8.0/Todo_App.AssemblyInfo.cs
+++ Todo_App/obj/Debug/net8.0/Todo_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c3381461477068e38ad5ade309e7e48b0b1585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcecff204387edca97a701df7af1593e87ae9a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
